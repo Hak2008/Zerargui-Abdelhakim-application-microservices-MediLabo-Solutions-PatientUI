71c610f8c322e55ecbbd3032a2b46df3cade0f54
Zerargui <hakim.zerargui@hotmail.com>
2024-03-29 15:30:46 +0100

    Mise en place de SpringSecurity, FeignConfig

---
+++ src/main/java/com/medilabosolutions/PatientUI/config/FeignConfig.java
@@ -0,0 +1,17 @@
+package com.medilabosolutions.PatientUI.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.RequestInterceptor;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("user", "user");
+    }
+}
+
